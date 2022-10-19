dacaf0358d7ed24a7b9aa898c1ec264b38563ccf
Batuhan <batuhan.kizil@outlook.com>
2022-10-19 12:59:14 +0300

    First Code

--- MomentoBank/cmake-build-debug/cmake_install.cmake
+++ MomentoBank/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/batu/CLionProjects/MomentoBank
+# Install script for directory: /Users/batu/Documents/MomentoBankSystem/MomentoBank
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/batu/CLionProjects/MomentoBank/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/batu/Documents/MomentoBankSystem/MomentoBank/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
