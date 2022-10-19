0822064ecdfaf91e1660ceb479b206213a3e8247
Batuhan <batuhan.kizil@outlook.com>
2022-10-19 13:17:07 +0300

    mac 2

--- MomentoBank/menu.cpp
+++ MomentoBank/menu.cpp
@@ -7,7 +7,7 @@
 void bank::menu() {
     int choice;
     cout<<"------------ Momento Bank ------------ \n\n 1 - Giriş Yapınız \n 2 - Hesap Oluştur \n 3 - Admin Giriş \n 4 - Çıkış \n\n ------------------------------------- \n\n";
-    cout<<"Giriş Yapınız : ";
+    cout<<"Giriş Yapınız :: ";
     cin>>choice;
 
     if(choice == 1){
